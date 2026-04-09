283a4971d6f68133e68dc675d3c8b36efa457137
barrychello <bintangcool16@gmail.com>
2026-04-09 18:02:19 +0800

    Halve the hero font size so text spans roughly 50% of the screen

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
       {/* Main Minimalist Typography */}
       <div className="flex-grow flex flex-col justify-center w-full mt-16 md:mt-0 z-10 relative">
         <h1 
-          className="text-[12vw] md:text-[10vw] xl:text-[10rem] font-bold uppercase leading-tight tracking-wide"
+          className="text-[6vw] md:text-[5vw] xl:text-[5rem] font-bold uppercase leading-tight tracking-wide"
           style={{ fontFamily: "'Boldonse', sans-serif" }}
         >
           CREATING TOMORROWS<br />
